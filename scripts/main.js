95f2d094ed4737a7312ae6dad65d4174576e606c
CalJersey <dan@vitad.com>
2017-06-25 22:03:44 -0700

    Added card objects

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,36 @@
  console.log("User flipped " + cardOne);
  console.log("User flipped " + cardThree); */
 
- var cards = ["queen","queen","king","king"];
+ var cards = [
+ 	{
+ 		rank: "queen",
+ 		suit: "hearts",
+ 		cardImage: "images/queen-of-hearts.png"
+
+ 	},
+ 	{
+ 		rank: "queen",
+ 		suit: "diamonds",
+ 		cardImage: "images/queen-of-diamonds.png"
+
+ 	},
+ 	{
+ 		rank: "king",
+ 		suit: "hearts",
+ 		cardImage: "images/king-of-hearts.png"
+
+ 	},
+ 	{
+ 		rank: "king",
+ 		suit: "diamonds",
+ 		cardImage: "images/queen-of-diamonds.png"
+
+ 	}
+ ];
  var cardsInPlay = [];
  
 function checkForMatch() {
- 	if(cardsInPlay[0] === cardsInPlay[1]){
+ 	if(cardsInPlay[0].rank === cardsInPlay[1].rank){
 		alert("You found a match!");
 	} else {
 		alert("Sorry. Try again!");
@@ -19,7 +44,9 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-	console.log("card flipped was", cards[cardId]);
+	console.log("card flipped is", cards[cardId].rank);
+	console.log("card suit is", cards[cardId].suit);
+	console.log("card image is", cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId]);
 	if (cardsInPlay.length === 2) {
 	 	checkForMatch();
