edbfd441514fb7f8160f31ce17b1d6b7479a267f
CalJersey <dan@vitad.com>
2017-06-25 20:19:10 -0700

    created game logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,23 @@
- var cardOne = "queen";
+/* var cardOne = "queen";
  var cardTwo = "queen";
  var cardThree = "king";
  var cardFour = "king";
 
  console.log("User flipped " + cardOne);
- console.log("User flipped " + cardThree);
+ console.log("User flipped " + cardThree); */
+
+ var cards = ["queen","queen","king","king"];
+ var cardsInPlay = [];
+ var cardOne = cards[0];
+ cardsInPlay.push(cardOne);
+ cardsInPlay.push(cards[2]);
+ 
+ console.log("User flipped" , cardsInPlay[0]);
+
+ if (cardsInPlay.length === 2) {
+ 	if(cardsInPlay[0] === cardsInPlay[1]){
+ 		alert("You found a match!")
+ 	} else {
+ 		alert("Sorry. Try again!")
+ 	}
+ }
